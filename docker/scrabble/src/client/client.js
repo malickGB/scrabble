6b52387e51a2e10fced603c1dacc1fd74a9154d5
malickGB <malick.grandbois@gmail.com>
2020-10-22 12:17:05 +0200

    multi: rooms implementation

--- docker/scrabble/src/client/client.js
+++ docker/scrabble/src/client/client.js
@@ -1,77 +1,85 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import socketIOClient from 'socket.io-client';
 import Game from '../game/game';
 import shortid from 'shortid';
+import './client.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Swal from 'sweetalert2';
+
 const ENDPOINT = "http://localhost:8080";
 
 
 var socket = socketIOClient(ENDPOINT);
 
-class Client extends React.Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            isPlaying: false
-        }
-        this.roomId = null;
-    }
+function Client(){
 
-    createGameHandler = () => {
-        this.roomId = shortid.generate().substring(0,6);
-        socket.emit('createRoom', this.roomId);
-        Swal.fire({
-            title: "Partagez ce code avec un ami",
-            text:  this.roomId,
-            allowOutsideClick: false,
-            confirmButtonText: "Quitter",
-            confirmButtonColor: "red"
-        }).then((res)=>{
-            if(res.isConfirmed){
-                socket.emit('leftRoomCreation', this.roomId);
-            }
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    useEffect(() => {
+        socket.on('GameStart', () =>{
+           setIsPlaying(prevState => !prevState);
+           Swal.close()
         })
-        // FIXME
-    }
-
-    joinGameHandler = () => {
-        Swal.fire({
-            title: 'Identifiant de partie',
-            input: 'text',
-            inputPlaceholder: 'Entrez un identifiant ',
-            inputAttributes: {
-                'aria-label': 'Type your message here'
-            },
-            showCancelButton: true,
-            inputValidator: (roomId) =>{
-                socket.emit('joinRoom', roomId)
-            }
-        })
-    }
-
-    render() {
-        if (!this.state.isPlaying){
-            return(
-                <div className="lobby">
+    })
+    
+    if (!isPlaying){
+        return(
+            <div className="lobby">
+                <div className="container">
+                    <h1>Bienvenue</h1>
                     <div className="buttons-container">
-                        <button className="btn btn-primary" onClick={this.createGameHandler.bind(this)}>Create</button>
-                        <button className="btn btn-primary" onClick={this.joinGameHandler.bind(this)}>Join</button>
+                        <button className="btn btn-primary" onClick={() => createGameHandler()}>Create</button>
+                        <button className="btn btn-primary" onClick={() => joinGameHandler()}>Join</button>
                     </div>
                 </div>
-            )
-        }
-        else{
-            return (
-                <div>
-
-                    <Game
-                        socket={socket}
-                    />
-                </div>
-            )
-        }
+            </div>
+        )
     }
+    else{
+        return (
+            <div>
+                <Game
+                    socket={socket}
+                />
+            </div>
+        )
+    }
+    
 }
+
+function createGameHandler(){
+    var roomId = shortid.generate().substring(0, 6);
+    socket.emit('createRoom', roomId);
+    Swal.fire({
+        title: "Partagez ce code avec un ami",
+        text: roomId,
+        allowOutsideClick: false,
+        confirmButtonText: "Quitter",
+        confirmButtonColor: "red"
+    }).then((res) => {
+        if (res.isConfirmed) {
+            socket.emit('leftRoomCreation', roomId);
+        }
+        console.log()
+    })
+    return roomId;
+}
+
+function joinGameHandler(){
+    Swal.fire({
+        title: 'Identifiant de partie',
+        input: 'text',
+        inputPlaceholder: 'Entrez un identifiant ',
+        inputAttributes: {
+            'aria-label': 'Type your message here'
+        },
+        showCancelButton: true,
+        inputValidator: (roomId) => {
+            socket.emit('joinRoom', roomId)
+
+        }
+    })
+}
+
 
 export default Client
--- docker/server/index.js
+++ docker/server/index.js
@@ -1,3 +1,5 @@
+const { clearLine } = require('readline');
+
 const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
@@ -7,50 +9,54 @@ const port = process.env.PORT || 8080;
 
 let players = [];
 let current_turn = true; //for player 1, false otherwise
-let data = { "player1": 0, "player2": 0, "board": Array(225).fill[null, null, null], "letters": {}}
-
+rooms_data = [];
 
 io.on("connection", (socket) =>{
-    // console.log("Player connected in room " + 1);
 
     socket.on('createRoom', (roomId) =>{
         socket.join(roomId);
         console.log("Player created room " + roomId);
     });
 
-    socket.on('joinRoom', (roomId) =>{
-        if (io.sockets.adapter.rooms[roomId]){
-            console.log("Player joined room "+ roomId);
-            socket.join(roomId);
-        }
-    })
-
     socket.on('leftRoomCreation', (roomId) => {
         socket.leave(roomId);
-        console.log("Player left room "+ roomId)
+        console.log("Player left room " + roomId)
     })
 
-    players.push(socket)
-    if (players.length == 2)
-    {
-        players[0].emit("initPlayers", true)
-    }
-    socket.on("endTurn",(board, letters) =>{
+    socket.on('joinRoom', (roomId) =>{
+        var connectedSockets = io.sockets.adapter.rooms[roomId].sockets;
+        if (io.sockets.adapter.rooms[roomId] && Object.keys(connectedSockets).length < 2){
+            console.log("Player joined room "+ roomId);
+            socket.join(roomId);
+            io.to(roomId).emit('GameStart');
+            var player1Id= Object.keys(connectedSockets)[0];
+            io.sockets.connected[player1Id].emit('initPlayers')
+            rooms_data[roomId] = { "player1": 0, "player2": 0, "board": Array(225).fill[null, null, null], "letters": {} }
+        }
+    })
+    
+    // FIXME : define roomId in front-end
+    socket.on("endTurn",(board, letters, roomId) =>{
+        data = rooms_data[roomId];
         data["board"] = board;
         data["letters"] = letters;
         socket.broadcast.emit("refreshScore", data)
     })
 
     //Updates scores
-    socket.on("newScore",(player,score) => {
+    socket.on("newScore",(player,score, roomId) => {
+        data = rooms_data[roomId];
         data[player] = score;
     })
 
     // Remove player from array
     socket.on("disconnecting", () =>{
         console.log(socket.id + " disconnected");
-        players.splice(players.indexOf(socket), 1)
-        current_turn = true;
+        var roomsSocketIn = Object.keys(socket.rooms);
+        roomsSocketIn.forEach( (room )=>{ 
+            socket.leave(room) ; 
+            io.to(room).emit('LeftGame'); // FIXME: do something with this event  
+        })
     })
 })
 
